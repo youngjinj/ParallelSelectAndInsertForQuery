8b774019f4d61f123bcd255967a59f43d6ba2b64
youngjinj <youngjinj@cubrid.com>
2023-05-09 21:52:34 +0900

    Update

--- ParallelSelectAndInsertForQuery/src/main/java/org/cubrid/ParallelQueryInsert.java
+++ ParallelSelectAndInsertForQuery/src/main/java/org/cubrid/ParallelQueryInsert.java
@@ -17,8 +17,26 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/*-
+ * Settings to use the below in eclipse'
+ *   - @formatter:off
+ *   - @formatter:on
+ * 
+ * Window -> Preferences -> Java -> Code Style -> Formatter -> Active profile: -> Edit...
+ *
+ *     Profile name: "Eclipse [built-in]" -> "Eclipse [built-in] [off/on tags]"
+ *     Off.On Tags: [v] Enable Off/On tags
+ *
+ * (Profile 'Eclipse [built-in]') -> OK
+ * (Preferences) -> Apply and Close
+ */
 
 public class ParallelQueryInsert {
+	public static final String PREFIX_THREAD_ID = "__t_";
+
+	public static final int LABEL_COUNT = 2;
 	public static final String LABEL_BEGIN = "begin";
 	public static final String LABEL_END = "end";
 
@@ -26,7 +44,17 @@ public class ParallelQueryInsert {
 	private static final String DB_USER = "dba";
 	private static final String DB_PASSWORD = "";
 
+	private static ConcurrentLinkedQueue<Map<String, String>> queue1;
+	private static ConcurrentLinkedQueue<Map<String, String>> queue2;
+	private static ConcurrentLinkedQueue<Map<String, String>> queue3;
+	private static ConcurrentLinkedQueue<Map<String, String>> queueWithThreadId;
+
+	static AtomicInteger atomicThreadId;
+
 	public static void main(String[] args) {
+		String query = null;
+		int numRepeat = 1;
+		int numThreads = 1;
 
 		Instant start = Instant.now();
 
@@ -36,17 +64,47 @@ public class ParallelQueryInsert {
 		create or replace procedure parallel_insert([query] varchar, thread_num int) as language java
 		name 'ParallelQueryInsert.ParallelQueryInsertInternal(java.lang.String, int)';
 		
-		drop table if exists t1, t2;
+		drop table if exists t1, t2, t3, t4, t4_1;
 		create table t1 (c1 varchar primary key, c2 varchar);
 		insert into t1 select 'A' || lpad (rownum, 7, '0'), 'B' || lpad (rownum, 7, '0') from db_class a, db_class b, db_class c, db_class d, db_class e limit 1000000;
 		create table t2 like t1;
+		create table t3 like t1;
+		create table t4 like t1;
+		alter table t4 add column thread_id varchar not null first;
+		create table t4_1 like t1;
+		
+		truncate table t2;
+		truncate table t3;
+		truncate table t4;
+		truncate table t4_1;
 		
 		call parallel_insert('insert into t2 select * from t1 where c1 between ? and ?', 10);
+		...
+		
+		select count(*) from (select * from t1 intersect select * from t2);
+		select count(*) from (select * from t1 intersect select * from t3);
+		select count(*) from (select * from t1 intersect select * from t4_1);
 		 */
 
-		String query = "insert into t2 select * from t1 where c1 between ? and ?";
-		int threadNum = 100;
-		ParallelQueryInsertInternal(query, threadNum);
+		numThreads = 100;
+
+		// @formatter:off
+		query = "insert into t2 select * from t1 where c1 between ? and ?";
+		numRepeat = 1;
+		// ParallelQueryInsertInternal(query, queue1, numRepeat, numThreads, false);
+
+		query = "insert into t3 select * from t1 where c1 between ? and ? and c1 between ? and ?";
+		numRepeat = 2;
+		// ParallelQueryInsertInternal(query, queue2, numRepeat, numThreads, false);
+
+		query = "insert into t4 select ?, t.* from t1 t where t.c1 between ? and ?";
+		numRepeat = 1;
+		ParallelQueryInsertInternal(query, queue3, numRepeat, numThreads, true);
+
+		query = "insert into t4_1 select t.c1, t.c2 from t4 t where t.thread_id between ? and ?";
+		numRepeat = 1;
+		ParallelQueryInsertInternal(query, queueWithThreadId, numRepeat, numThreads, false);
+		// @formatter:on
 
 		Instant end = Instant.now();
 		Duration duration = Duration.between(start, end);
@@ -64,77 +122,132 @@ public class ParallelQueryInsert {
 		System.out.println("");
 	}
 
-	public static void ParallelQueryInsertInternal(String paramQuery, int paramNumThreads) {
-		/*-
-		 * Bind variables 
-		 */
-		ConcurrentLinkedQueue<Map<String, String>> queue = new ConcurrentLinkedQueue<Map<String, String>>() {
-			{
-				offer(new HashMap<String, String>() {
-					{
-						put(LABEL_BEGIN, "A0000001");
-						put(LABEL_END, "A0100000");
-					}
-				});
-				offer(new HashMap<String, String>() {
-					{
-						put(LABEL_BEGIN, "A0100001");
-						put(LABEL_END, "A0200000");
-					}
-				});
-				offer(new HashMap<String, String>() {
-					{
-						put(LABEL_BEGIN, "A0200001");
-						put(LABEL_END, "A0300000");
-					}
-				});
-				offer(new HashMap<String, String>() {
-					{
-						put(LABEL_BEGIN, "A0300001");
-						put(LABEL_END, "A0400000");
-					}
-				});
-				offer(new HashMap<String, String>() {
-					{
-						put(LABEL_BEGIN, "A0400001");
-						put(LABEL_END, "A0500000");
-					}
-				});
-				offer(new HashMap<String, String>() {
-					{
-						put(LABEL_BEGIN, "A0500001");
-						put(LABEL_END, "A0600000");
-					}
-				});
-				offer(new HashMap<String, String>() {
-					{
-						put(LABEL_BEGIN, "A0600001");
-						put(LABEL_END, "A0700000");
-					}
-				});
-				offer(new HashMap<String, String>() {
-					{
-						put(LABEL_BEGIN, "A0700001");
-						put(LABEL_END, "A0800000");
-					}
-				});
-				offer(new HashMap<String, String>() {
-					{
-						put(LABEL_BEGIN, "A0800001");
-						put(LABEL_END, "A0900000");
-					}
-				});
-				offer(new HashMap<String, String>() {
-					{
-						put(LABEL_BEGIN, "A0900001");
-						put(LABEL_END, "A1000000");
-					}
-				});
-			}
-		};
+	static {
+		queue1 = new ConcurrentLinkedQueue<Map<String, String>>();
+		queue2 = new ConcurrentLinkedQueue<Map<String, String>>();
+		queue3 = new ConcurrentLinkedQueue<Map<String, String>>();
+		queueWithThreadId = new ConcurrentLinkedQueue<Map<String, String>>();
 
-		String query = paramQuery;
+		makeQueue1();
+		makeQueue2();
+		makeQueue3();
+		makeQueue4();
+
+		atomicThreadId = new AtomicInteger(1);
+	}
+
+	private static void makeQueue1() {
+		// @formatter:off
+		String[][] valueList = {
+				{ "A0000001", "A0100000" },
+				{ "A0100001", "A0200000" },
+				{ "A0200001", "A0300000" },
+				{ "A0300001", "A0400000" },
+				{ "A0400001", "A0500000" },
+				{ "A0500001", "A0600000" },
+				{ "A0600001", "A0700000" },
+				{ "A0700001", "A0800000" },
+				{ "A0800001", "A0900000" },
+				{ "A0900001", "A1000000" } };
+		// @formatter:on
+
+		for (int i = 0; i < valueList.length; i++) {
+			if (valueList[i].length == LABEL_COUNT) {
+				Map<String, String> map = new HashMap<String, String>();
+				map.put(LABEL_BEGIN, valueList[i][0]);
+				map.put(LABEL_END, valueList[i][1]);
+				queue1.offer(map);
+			}
+		}
+	}
+
+	private static void makeQueue2() {
+		// @formatter:off
+		String[][] valueList = {
+				{ "A0000001", "A0100000" },
+				{ "A0100001", "A0200000" },
+				{ "A0200001", "A0300000" },
+				{ "A0300001", "A0400000" },
+				{ "A0400001", "A0500000" },
+				{ "A0500001", "A0600000" },
+				{ "A0600001", "A0700000" },
+				{ "A0700001", "A0800000" },
+				{ "A0800001", "A0900000" },
+				{ "A0900001", "A1000000" } };
+		// @formatter:on
+
+		for (int i = 0; i < valueList.length; i++) {
+			if (valueList[i].length == LABEL_COUNT) {
+				Map<String, String> map = new HashMap<String, String>();
+				map.put(LABEL_BEGIN, valueList[i][0]);
+				map.put(LABEL_END, valueList[i][1]);
+				queue2.offer(map);
+			}
+		}
+	}
+
+	private static void makeQueue3() {
+		// @formatter:off
+		String[][] valueList = {
+				{ "A0000001", "A0100000" },
+				{ "A0100001", "A0200000" },
+				{ "A0200001", "A0300000" },
+				{ "A0300001", "A0400000" },
+				{ "A0400001", "A0500000" },
+				{ "A0500001", "A0600000" },
+				{ "A0600001", "A0700000" },
+				{ "A0700001", "A0800000" },
+				{ "A0800001", "A0900000" },
+				{ "A0900001", "A1000000" } };
+		// @formatter:on
+
+		for (int i = 0; i < valueList.length; i++) {
+			if (valueList[i].length == LABEL_COUNT) {
+				Map<String, String> map = new HashMap<String, String>();
+				map.put(LABEL_BEGIN, valueList[i][0]);
+				map.put(LABEL_END, valueList[i][1]);
+				queue3.offer(map);
+			}
+		}
+	}
+
+	private static void makeQueue4() {
+		// @formatter:off
+		String[][] valueList = {
+				{ "1", "1" },
+				{ "2", "2" },
+				{ "3", "3" },
+				{ "4", "4" },
+				{ "5", "5" },
+				{ "6", "6" },
+				{ "7", "7" },
+				{ "8", "8" },
+				{ "9", "9" },
+				{ "10", "10" },
+				{ "11", "11" },
+				{ "12", "12" } };
+		// @formatter:on
+
+		for (int i = 0; i < valueList.length; i++) {
+			if (valueList[i].length == LABEL_COUNT) {
+				Map<String, String> map = new HashMap<String, String>();
+				map.put(LABEL_BEGIN, PREFIX_THREAD_ID + valueList[i][0]);
+				map.put(LABEL_END, PREFIX_THREAD_ID + valueList[i][1]);
+				queueWithThreadId.offer(map);
+			}
+		}
+	}
+
+	public static void ParallelQueryInsertInternal(String query, ConcurrentLinkedQueue<Map<String, String>> queue,
+			int paramNumRepeat, int paramNumThreads, boolean insertThreadId) {
+		int numRepeat = 1;
 		int numThreads = 1;
+
+		if (paramNumRepeat < 0) {
+			numRepeat = 1;
+		} else {
+			numRepeat = paramNumRepeat;
+		}
 
 		int availableProcessors = Runtime.getRuntime().availableProcessors();
 		if (paramNumThreads > availableProcessors) {
@@ -165,8 +278,15 @@ public class ParallelQueryInsert {
 		}
 
 		List<InsertCallable> insertCallableList = new ArrayList<>();
-		for (int i = 0; i < numThreads; i++) {
-			insertCallableList.add(new InsertCallable(connectionList.get(i), query, queue));
+
+		if (insertThreadId) {
+			for (int i = 0; i < numThreads; i++) {
+				insertCallableList.add(new InsertCallable(connectionList.get(i), query, queue, numRepeat, atomicThreadId.getAndIncrement()));
+			}
+		} else {
+			for (int i = 0; i < numThreads; i++) {
+				insertCallableList.add(new InsertCallable(connectionList.get(i), query, queue, numRepeat));
+			}
 		}
 
 		boolean needRollback = false;
@@ -277,11 +397,25 @@ class InsertCallable implements Callable<Void> {
 	private Connection connection;
 	private String query;
 	private ConcurrentLinkedQueue<Map<String, String>> queue;
+	private int numRepeat;
+	private String threadIdWithPrefix;
 
-	public InsertCallable(Connection connection, String query, ConcurrentLinkedQueue<Map<String, String>> queue) {
+	public InsertCallable(Connection connection, String query, ConcurrentLinkedQueue<Map<String, String>> queue,
+			int numRepeat) {
 		this.connection = connection;
 		this.query = query;
 		this.queue = queue;
+		this.numRepeat = numRepeat;
+		this.threadIdWithPrefix = null;
+	}
+
+	public InsertCallable(Connection connection, String query, ConcurrentLinkedQueue<Map<String, String>> queue,
+			int numRepeat, long threadId) {
+		this.connection = connection;
+		this.query = query;
+		this.queue = queue;
+		this.numRepeat = numRepeat;
+		this.threadIdWithPrefix = ParallelQueryInsert.PREFIX_THREAD_ID + threadId;
 	}
 
 	@Override
@@ -291,8 +425,19 @@ class InsertCallable implements Callable<Void> {
 				Map<String, String> work = queue.poll();
 
 				if (work != null) {
-					statement.setString(1, work.get(ParallelQueryInsert.LABEL_BEGIN));
-					statement.setString(2, work.get(ParallelQueryInsert.LABEL_END));
+					int i = 1;
+					int max = numRepeat * ParallelQueryInsert.LABEL_COUNT;
+
+					if (threadIdWithPrefix != null) {
+						statement.setString(i, threadIdWithPrefix);
+						i++;
+						max++;
+					}
+
+					for (; i < max; i += ParallelQueryInsert.LABEL_COUNT) {
+						statement.setString(i, work.get(ParallelQueryInsert.LABEL_BEGIN));
+						statement.setString(i + 1, work.get(ParallelQueryInsert.LABEL_END));
+					}
 				} else {
 					break;
 				}
